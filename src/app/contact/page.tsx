60543732de93014ca3a4518791e11a66a823a26e
John Bieniek <1126076+JohnBieniek@users.noreply.github.com>
2026-09-06 19:59:29 -0400

    Rename transfer service to Website Transfer

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -50,7 +50,7 @@ export default function ContactPage() {
             <p>Business Consulting Session <b>$300</b></p>
             <p>Brand &amp; Advertising Plan <b>$1,000</b></p>
             <p>Website Support <b>$100/hour</b></p>
-            <p>Domain Change &amp; Website Transfer <b>$750</b></p>
+            <p>Website Transfer <b>$750</b></p>
             <p>Single-Page Website <b>$750</b></p>
             <p>Five-Page Website <b>$2,500</b></p>
           </div>
